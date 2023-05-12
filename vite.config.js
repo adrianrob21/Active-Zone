c0bf1e1a3314e621b93cff08bb97af8e3726a4ab
Rob <adirobadi03@gmail.com>
2023-05-13 01:35:37 +0300

    pwa test

--- vite.config.js
+++ vite.config.js
@@ -34,22 +34,22 @@ export default defineConfig({
         theme_color: "#FFD700",
         icons: [
           {
-            src: "/public/pwa-192x192.png",
+            src: "./public/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/public/pwa-256x256.png",
+            src: "./public/pwa-256x256.png",
             sizes: "256x256",
             type: "image/png",
           },
           {
-            src: "/public/pwa-512x512.png",
+            src: "./public/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/public/pwa-256x256.png",
+            src: "./public/pwa-256x256.png",
             sizes: "256x256",
             type: "image/png",
             purpose: "any maskable",
